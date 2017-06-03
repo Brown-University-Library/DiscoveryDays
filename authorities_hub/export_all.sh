7107f294bca4ea04ca370880799c5880b27082f1
Hector Correa <hector_correa@brown.edu>
2017-06-03 18:36:20 -0400

    Minor tweaks to the samples to export the data

--- authorities_hub/export_all.sh
+++ authorities_hub/export_all.sh
@@ -1,1 +1,4 @@
-curl -H "Accept: text/turtle" -X POST -d @select_all_graph.sparql http://localhost:3030/testdb/sparql > ./data/demo_triples.ttl
+# exports all data as a Graph 
+curl -H "Accept: text/turtle" \
+  -X POST \
+  -d @select_all_graph.sparql http://localhost:3030/testdb/sparql
---
+++ authorities_hub/export_all2.sh
@@ -0,0 +1,2 @@
+# simple cURL (one-liner)
+curl "http://localhost:3030/testdb/sparql?query=SELECT+?subject+?predicate+?object+WHERE+\{++?subject+?predicate+?object+\}"
